7345b8b58951b125e333b29902ce55306a83ac73
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-27 23:34:11 +0600

    addad style and dynamic name , email and phone number

--- src/Users.jsx
+++ src/Users.jsx
@@ -3,13 +3,13 @@ import { use } from "react";
 import "./App.css";
 export default function Users({ userData }) {
   const users = use(userData);
-  console.log(users);
+  // console.log(users);
 
   return (
     <div className="card">
       <h3>users:{users.length} </h3>
       {users.map((user) => (
-        <User user={user}></User>
+        <User key={user.id} user={user}></User>
       ))}
     </div>
   );
--- src/user.jsx
+++ src/user.jsx
@@ -1,7 +1,7 @@
 export default function User({ user }) {
   const { name, email, phone } = user;
   return (
-    <div className="card">
+    <div className="cards">
       <h4>name: {name}</h4>
       <p>email: {email}</p>
       <p>phone: {phone}</p>
